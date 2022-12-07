c468fdc059a3d2acecf9b5af4162fd8f5fe0b5ad
blazk23 <120020106+blazk23@users.noreply.github.com>
2022-12-07 14:54:28 +0100

    Update Vaja6.sh

--- Vaja6.sh
+++ Vaja6.sh
@@ -2,4 +2,6 @@
 
 mkdir WINDOWS
 cd WINDOWS
-mkdir Desktop, Documents, Downloads, Pictures, Videos
+mkdir Desktop Documents Downloads Pictures Videos
+cd Documents
+$ for i in {1..5}; do mkdir $i_folder ; done
